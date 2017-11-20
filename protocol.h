0d35a59f58f55b66ff67dac6b12acf3ab3bf5f00
julianweihe <julian.weihe@gmx.de>
2017-11-20 16:27:12 +0100

    Text

---
+++ protocol.h
@@ -0,0 +1,17 @@
+//
+// Created by aadc on 20.11.17.
+//
+
+#ifndef PROJECT_PROTOCOL_H
+#define PROJECT_PROTOCOL_H
+
+#include <stdint.h>
+
+#define valueToMessage(m) ((void*)(uint64_t)m)
+#define messageToValue(v) ((uint64_t)v)
+
+
+
+
+
+#endif //PROJECT_PROTOCOL_H
